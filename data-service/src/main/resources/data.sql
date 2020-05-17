e399daf02476651eeff65f61c3894d8637c33bd1
Syed Ehsan Ali Razvi <ehsan.razvi92@gmail.com>
2020-05-17 08:35:32 +0530

    Data.sql added

---
+++ data-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values (1,"syed@gmail.com","Syed","syed123","ROLE_ADMIN");
+insert into user values (2,"maruthi@gmail.com","Maruthi","maruthi123","ROLE_ADMIN");
+insert into user values (3,"rekha@gmail.com","Rekha","rekha123","ROLE_ADMIN");
+insert into user values (4,"kalpana@gmail.com","Kalpana","kalpana123","ROLE_ADMIN");
